368a667389f69db739c89d745dec97298b11f078
Rift <61116182+Riftaricus@users.noreply.github.com>
2026-09-17 13:15:42 +0200

    Added factorypattern and did the excercise

---
+++ FactoryPattern/CaliforniaPizzaFactory.cs
@@ -0,0 +1,29 @@
+﻿namespace SimpleFactory
+{
+    internal class CaliforniaPizzaFactory : SimplePizzaFactory
+    {
+        public virtual Pizza CreatePizza(string type)
+        {
+            Pizza pizza = null;
+
+            if (type.Equals("cheese"))
+            {
+                pizza = new CaliforniaStyleCheesePizza();
+            }
+            else if (type.Equals("pepperoni"))
+            {
+                pizza = new CaliforniaStylePepperoniPizza();
+            }
+            else if (type.Equals("clam"))
+            {
+                pizza = new CaliforniaStyleClamPizza();
+            }
+            else if (type.Equals("veggie"))
+            {
+                pizza = new CaliforniaStyleVeggiePizza();
+            }
+
+            return pizza;
+        }
+    }
+}
---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace SimpleFactory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            NYPizzaFactory nyFactory = new NYPizzaFactory();
+            PizzaStore nyStore = new PizzaStore(nyFactory);
+            nyStore.OrderPizza("veggie");
+
+            ChicagoPizzaFactory chicagoFactory = new ChicagoPizzaFactory();
+            PizzaStore chicagoStore = new PizzaStore(chicagoFactory);
+            chicagoStore.OrderPizza("veggie");
+
+            CaliforniaPizzaFactory californiaFactory = new CaliforniaPizzaFactory();
+            PizzaStore californiaStore = new PizzaStore(californiaFactory);
+            californiaStore.OrderPizza("veggie");
+
+
+        }
+    }
+}
